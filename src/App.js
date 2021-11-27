3cd14ca230fc1096745dff5c85086d4980de8525
SACHIN <sachinshanmugam07@gmail.com>
2021-11-27 17:55:03 +0530

    Created repository for employee management using react hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,150 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState} from "react";
+import Axios from 'axios';
 
 function App() {
+  const [empId, setEmpId] = useState(0);
+  const [firstName, setfirstName] = useState("");
+  const [surName, setsurName] = useState("");
+  const [email, setEmail] = useState("");
+  const [dob, setDob] = useState(0);
+  const [gender, setGender] = useState("");
+
+  
+const createEmp =() =>{
+  console.log(empId);
+Axios.post('http://localhost:3000/employees', {
+  empId : empId,
+ firstName: firstName,
+ surName:surName, 
+ email:email,
+ dob: dob,
+ gender: gender
+})
+.then(()=>{
+  console.log("success");
+});
+};
+
+const readEmp =() =>{
+  Axios.get('http://localhost:3000/employees/'+empId).then((res)=>{
+  console.log(res);
+},
+(error) => {
+console.log(error);
+})
+}
+const updateEmp = () =>{
+   Axios.put(('http://localhost:3000/employees/'+empId),{
+    firstName,surName,email,dob,gender }).then((res)=>{
+  console.log(res);
+},
+(error) => {
+console.log(error);
+})
+
+}
+const deleteEmp =() =>{
+  Axios.delete('http://localhost:3000/employees/'+empId).then((res)=>{
+  console.log(res);
+},
+(error) => {
+console.log(error);
+})
+}  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        
+        <h1>Employee Management </h1> <hr />
+        <button onClick={createEmp} >Create</button>
+        <button onClick={readEmp} >Read</button>
+        <button onClick={updateEmp} >Update</button>
+        <button onClick={deleteEmp} >Delete</button> <hr />
+       <br /> <hr />
+       </div>
+       <div className='information'>
+       <table>
+        <tbody>
+      <tr>
+      <td> 
+        <label> Employee ID  : </label>
+        </td>
+      <td>
+        <input type="text" onChange={(event)=>{
+          setEmpId(event.target.value);
+        }}/>
+        </td>
+        
+      </tr>
+      </tbody>
+      <tbody>
+      <tr>
+      
+      <td> 
+        <label> First Name   : </label>
+        </td>
+      <td>
+        <input type="text" onChange={(event)=>{
+          setfirstName(event.target.value);
+        }}/>
+        </td>
+        
+      </tr>
+      </tbody>
+      <tbody>
+      <tr>
+      <td> 
+        <label> Last Name  :</label>
+        </td>
+      <td>
+        <input type="text" onChange={(event)=>{
+          setsurName(event.target.value);
+        }}/>
+        </td>
+      </tr>
+      </tbody>
+      <tbody><tr>
+      <td> 
+        <label> E-Mail  :</label>
+        </td>
+      <td>
+        <input type="text" onChange={(event)=>{
+          setEmail(event.target.value);
+        }}/>
+        </td>
+      </tr>
+      </tbody>
+      <tbody><tr>
+      <td> 
+        <label> DOB  :</label>
+        </td>
+      <td>
+        <input type="date" onChange={(event)=>{
+          setDob(event.target.value);
+        }}/>
+        </td>
+      </tr>
+      </tbody>
+      <tbody>
+      <tr>
+      <td> 
+        <label> Gender  :</label>
+        </td>
+      <td>
+        <input style={{fontSize:20}} type="radio" onChange={(event)=>{
+          setGender(event.target.value);
+        }}/>
+        <label> Male </label>
+        <input type="radio" value="Female" onChange={(event)=>{
+          setGender(event.target.value);
+        }}/>
+        <label> Female</label>
+        </td>
+      </tr>
+      </tbody>
+            </table>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const GENDER_DEFAULT='';
+const EMPID_DEFAULT=0;
+const FIRSTNAME_DEFAULT='';
+const SURNAME_DEFAULT='';
+const EMAIL_DEFAULT = '';
+const DOB_DEFAULT='';
+const HIDE_EMPID_DEFAULT=true;
+const STATUS_DEFAULT='Create New Employee';
+const BUTTON_STATE_DEFAULT = 'Create';
+const EMAIL_ERROR_DRFAULT='';
+
 ReactDOM.render(
+
   <React.StrictMode>
-    <App />
+    <App gender={GENDER_DEFAULT}  empId={EMPID_DEFAULT}  firstName={FIRSTNAME_DEFAULT}  surName={SURNAME_DEFAULT} email={EMAIL_DEFAULT} dob={DOB_DEFAULT}  hideEmpId={HIDE_EMPID_DEFAULT}  status={STATUS_DEFAULT}  buttonState={BUTTON_STATE_DEFAULT}  emailError={EMAIL_ERROR_DRFAULT}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
